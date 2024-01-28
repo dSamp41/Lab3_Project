fe20588271ce64050c4120da008a83dc3e72f075
dSamp41 <d.sampietro@studenti.unipi.it>
2024-01-28 16:19:26 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@ rm src/server/*.class
 rm src/structures/*.class
 
 compilerOptions="javac -cp .:lib/gson-2.10.1.jar"
-$compilerOptions src/client/*.java
-$compilerOptions src/server/*.java
-$compilerOptions src/structures/*.java
+$compilerOptions -d build/client src/client/*.java
+$compilerOptions  -d build/server src/server/*.java
+$compilerOptions  -d build/structures src/structures/*.java
