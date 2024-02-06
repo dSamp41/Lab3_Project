0b5fc084444eee2d7ba0da87c40eeb711b21bb1f
dSamp41 <d.sampietro@studenti.unipi.it>
2024-02-06 15:57:19 +0100

    Update build process

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 cd build
 jar cfe client.jar src.client.ClientMain src/client/*.class
 #jar cfm server.jar serverManifest.txt src/server/*.class src/structures/*.class gson-2.10.1.jar
-./buildServer.sh
+../buildServer.sh
 
 mv client.jar ../artifacts
 mv server.jar ../artifacts
